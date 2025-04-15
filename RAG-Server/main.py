dc1808158641cd44758a40d3c7bba75beb1a1bec
Letícia Coelho <leticiamendes2122@gmail.com>
2025-04-15 13:28:21 +0100

    docs(rag-server): add inline comments for better readability and comprehension

--- RAG-Server/main.py
+++ RAG-Server/main.py
@@ -10,10 +10,14 @@ print("Model:", os.getenv("OPENAI_MODEL"))
 
 app = FastAPI()
 
+#Defines de expected format of the resquest body.
 class QueryRequest(BaseModel):
     file_id: str
     question: str
 
+# FastAPI route - entry point for when "user sends a request".
+# When someone sends a POST request to the /query endpoint with a body that matches QueryRequest,
+# the query function will be called.
 @app.post("/query")
 async def query(request: QueryRequest):
     try:
--- RAG-Server/query_engine.py
+++ RAG-Server/query_engine.py
@@ -14,15 +14,20 @@ llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
 
 
 def query_pdf(file_id: str, question: str) -> str:
-    file_path = os.path.join(PDF_FOLDER, file_id)
+    file_path = os.path.join(PDF_FOLDER, file_id) # Construct the full path to the PDF file(file_id + PDF_FOLDER from .env)
 
+    # Check if the file exists
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"❌ File not found: {file_path}")
 
     loader = PyPDFLoader(file_path)
+
+    # Load the PDF file - this will read the content of the PDF and convert it into a format that can be processed by LangChain.
+    # The loader will extract text from the PDF and create a list of documents.
     docs = loader.load()
 
-    chain = load_qa_chain(llm, chain_type="stuff")
-    result = chain.run(input_documents=docs, question=question)
+    # Build the prompt for the LLM
+    chain = load_qa_chain(llm, chain_type="stuff") # Loads a prebuilt chain template for question answering.
+    result = chain.run(input_documents=docs, question=question) # Runs the chain with the loaded documents and the user's question.
 
     return result
